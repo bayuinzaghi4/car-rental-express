f2c6b8aa3c7c62c77136d474338b49baed8ab9b2
bayuinzaghi4 <bayu4inzaghi@gmail.com>
2024-11-08 15:52:00 +0700

    Update index.js
    adding fullname

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -10,6 +10,7 @@ const router = express.Router();
 const user = new UserModel();
 
 const signUpSchema = Joi.object({
+  fullname: Joi.string().required(),
   email: Joi.string().email().required(),
   password: Joi.string()
     .min(8)
@@ -73,12 +74,13 @@ class AuthController extends BaseController {
 
   signUp = async (req, res, next) => {
     try {
-      const { email, password } = req.body;
+      const { fullname, email, password } = req.body;
       const user = await this.model.getOne({ where: { email } });
 
       if (user) return next(new ValidationError("Email already exist!"));
 
       const newUser = await this.model.set({
+        fullname,
         email,
         password: await encryptPassword(password),
         roleId: 3
